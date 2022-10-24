c2744e736be8299b96715e6afe69ee01cd7d2be7
pushpasahitya <pushpasahitya123@gmail.com>
2022-10-24 14:06:38 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BookComponent } from './book/book.component';
+import { HomeComponent } from './home/home.component';
+import { PackagesComponent } from './packages/packages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"packages",component:PackagesComponent},
+  {path:"book",component:BookComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
